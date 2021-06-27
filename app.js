2b0f1ef1839fc79dc3d67d29804ba89e8c2c6ab4
Mia Schultink <mia@schultink.com>
2021-06-27 18:11:34 +0300

    dsfgfsdg

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/login', loginRoutes);
 
 app.use('/', (req, res, next) => {
-    res.render('index', {
+    res.status(200).render('index', {
         pageTitle: 'Home page',
         path: '/'
     });
@@ -41,4 +41,4 @@ app.use((req, res, next) => {
     });
 });
 
-//app.listen(3000);
+app.listen(3000);
