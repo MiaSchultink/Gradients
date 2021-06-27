9d1b82b55ac2a0185df89f063dc7e96b2f753e21
Mia Schultink <mia@schultink.com>
2021-06-27 12:25:05 +0300

    fixes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const fs = require('fs');
-const espress = require('express');
+const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs')
@@ -10,11 +10,28 @@ app.set('views', 'views')
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/chess', (req,res, next)=>{
+    console.log('hello')
+    res.render('chess');
+});
+app.use('/login', (req,res, next)=>{
+  res.render('login',{
+      pageTitle: 'Log-in',
+      path: '/'
+  });
+});
+
+app.use((req,res, next)=>{
+    console.log('hello')
+    res.render('index');
+});
+
+
+
 
 
 app.use((req, res, next) => {
     res.status(404).render('404', { pageTitle: 'page not found' });
-
 });
 
 app.listen(3000)
