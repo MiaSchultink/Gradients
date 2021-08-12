eac8e723e0ccb50119f47fb86995a483bfd1f2d3
Mia Schultink <mia@schultink.com>
2021-08-12 14:58:05 +0300

    changes

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,8 @@
 const User = require('../models/user')
 const Gradient = require('../models/gradient')
+const user = require('../models/user')
+
+const mongoose = require('mongoose');
 
 exports.getAllUsers = async (req, res, next) =>{
     const users = await User.find().exec()
@@ -22,7 +25,37 @@ exports.getUser = async(req, res, next)=>{
     })
 }
 
-// exports.deleteGradient = async (req, res, next) =>{
-//     const gradient = await Gradient.findById(req.params.gradientId).exec()
-//     await gradient.remove()
-// }
+
+exports.deleteDuplicates = async (req, res, next)=>{
+    const user = await User.findById('60f147e8302a5323fc741c2c').populate('gradients').exec()
+    const posts = user.gradients;
+
+    const duplicates = [];
+db.collectionName.aggregate([
+  { $match: { 
+    name: { "$ne": '' }  // discard selection criteria
+  }},
+  { $group: { 
+    _id: { name: "$name"}, // can be grouped on multiple properties 
+    dups: { "$addToSet": "$_id" }, 
+    count: { "$sum": 1 } 
+  }}, 
+  { $match: { 
+    count: { "$gt": 1 }    // Duplicates considered as count greater than one
+  }}
+],
+{allowDiskUse: true}       // For faster processing if set is larger
+)               // You can display result until this and check duplicates 
+.forEach(function(doc) {
+    doc.dups.shift();      // First element skipped for deleting
+    doc.dups.forEach( function(dupId){ 
+        duplicates.push(dupId);   // Getting all duplicate ids
+        }
+    )    
+})
+// If you want to Check all "_id" which you are deleting else print statement not needed
+printjson(duplicates);     
+// Remove all duplicates in one go    
+db.collectionName.remove({_id:{$in:duplicates}})  
+     
+}
